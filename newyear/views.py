a05c4af936adbbe063f8c7442fe860483b1bbdb4
waghan2 <wagnerufpr@gmail.com>
2022-05-30 17:47:47 -0300

    Atualização da aula 3

--- newyear/views.py
+++ newyear/views.py
@@ -5,5 +5,7 @@ from django.shortcuts import render
 def index(request):
     now = datetime.datetime.now()
     return render(request, "newyear/index.html", {
-        "newyear": now.month == 1 and now.day == 1
+        "newyear": now.month == 1 and now.day == 1 # esta função retorna true se o mes atual for igual a jan, e dia =  1. Voce pode testear retornando true para ver se muda para yes no html.
     })
+    
+    
